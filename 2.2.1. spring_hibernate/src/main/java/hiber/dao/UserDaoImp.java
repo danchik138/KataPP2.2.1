0df20731232853f09a502765eed4a990e52b9187
danchik138 <danchik138@gmail.com>
2022-12-13 12:10:17 +0300

    DAO realization through EntityManager, renamed dao/service methods, hql request moved to constants

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,41 +1,41 @@
 package hiber.dao;
 
 import hiber.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+   @PersistenceContext
+   private EntityManager entityManager;
+
+   private static final String USER_BY_MODEL_AND_SERIES =
+           "FROM User where car.model = :model and car.series = :series";
+
+   private static final String GET_ALL_FROM_USERS =
+           "FROM User";
 
    @Override
    public User getUserByCarModelAndSeries(String model, int series) {
-      String hql = "FROM User where car.model = :model and car.series = :series";
-      Query query = sessionFactory
-              .getCurrentSession()
-              .createQuery(hql, User.class);
+      Query query = entityManager.createQuery(USER_BY_MODEL_AND_SERIES, User.class);
       query.setParameter("model", model);
       query.setParameter("series", series);
       return (User) query.getSingleResult();
    }
 
    @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().persist(user);
+   public void addUser(User user) {
+      entityManager.persist(user);
    }
 
    @Override
-   public List<User> listUsers() {
-      return sessionFactory
-              .getCurrentSession()
-              .createQuery("from User", User.class)
+   public List<User> getUsersList() {
+      return entityManager
+              .createQuery(GET_ALL_FROM_USERS, User.class)
               .getResultList();
    }
 
