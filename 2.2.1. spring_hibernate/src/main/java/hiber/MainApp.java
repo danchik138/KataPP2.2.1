508996af7b635654c0798af861f1d5f5e67db729
danchik138 <danchik138@gmail.com>
2022-12-15 08:38:21 +0300

    fixed UserService method names

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -20,12 +20,12 @@ public class MainApp {
       Car car3 = new Car("lamborghini3", 1007);
       Car car4 = new Car("lamborghini4", 1007);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.addUser(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.addUser(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.addUser(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.addUser(new User("User4", "Lastname4", "user4@mail.ru"));
 
-      List<User> users = userService.listUsers();
+      List<User> users = userService.getUsersList();
       for (User user : users) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
